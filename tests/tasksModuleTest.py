f94875c2a40c31109ff18544914ba88e0a4eed23
inktrap <inktrap@inktrap.org>
2016-10-16 01:04:03 +0200

    added test skeleton for tasks

---
+++ tests/tasksModuleTest.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3.5
+
+import unittest
+from modules import tasks_module
+from modules.config import thisConfig
+import os
+
+import logging
+logger = logging.getLogger('getTasksTest.py')
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s', '%m-%d %H:%M:%S')
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+class TestTimeout(unittest.TestCase):
+    def test_timeout(self):
+        #def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+        self.assertFalse()
+
+
+class TestCheckRestriction(unittest.TestCase):
+    def test_check_restriction(self):
+        #def check_restriction(settings, restriction):
+        self.assertFalse()
+
+
+class TestCheckTask(unittest.TestCase):
+    def test_check_task(self):
+        #def check_task(settings, task):
+        self.assertFalse()
+
+
+class TestGetSelectRestrictions(unittest.TestCase):
+    def test_get_select_restrictions(self):
+        #def get_select_restrictions(restrictions):
+        self.assertFalse()
+
+
+class TestCheckSelect(unittest.TestCase):
+    def test_check_select(self):
+        #def check_select(settings, select_restrictions):
+        self.assertFalse()
+
+
+class TestApplySuccessor(unittest.TestCase):
+    def test_apply_successor(self):
+        #def apply_successor(sample, successor_restrictions):
+        self.assertFalse()
+
+
+class TestApplySelect(unittest.TestCase):
+    def test_apply_select(self):
+        #def apply_select(settings, select_restrictions, tasks):
+        self.assertFalse()
+
+
+class TestCheckConfig(unittest.TestCase):
+    def test_check_config(self):
+        #check_config(settings, tasks):
+        self.assertFalse()
+
